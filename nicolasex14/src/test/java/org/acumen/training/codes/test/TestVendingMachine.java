060aa8770f280a685b926fbe3f51fd46c8225ebe
sanicolas <shainaanicolas@gmail.com>
2024-10-17 10:30:55 +0800

    modified pom.xml and added fixtures on TestVendingMachine

--- nicolasex14/src/test/java/org/acumen/training/codes/test/TestVendingMachine.java
+++ nicolasex14/src/test/java/org/acumen/training/codes/test/TestVendingMachine.java
@@ -1,19 +1,32 @@
 package org.acumen.training.codes.test;
 
+import org.acumen.training.codes.itemd.TrapArray;
 import org.acumen.training.codes.itemf.Coin;
 import org.acumen.training.codes.itemf.VendingMachine;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestVendingMachine {
 	private static final Logger LOGGER = LogManager.getLogger(TestVendingMachine.class);
-
+	VendingMachine vm;
+	
+	@BeforeEach
+	public void setUp() {
+		vm = new VendingMachine();
+	}
+	
+	@AfterEach
+	public void teardown() {
+		vm = null;
+	}
+	
 	@Test
 	public void testVendingMachine() {
 		LOGGER.info("TestVendingMachine started...");
-
-		VendingMachine vm = new VendingMachine();
+		
 		Coin penny = new Coin("Penny");
 		Coin nickle = new Coin("Nickle");
 		Coin invalid = new Coin("RandomInvalidCoin");
